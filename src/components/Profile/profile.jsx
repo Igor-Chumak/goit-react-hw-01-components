2f1fb5a29e7d0502a02f5be41c943de5adcdc97c
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-13 01:51:05 +0300

    markup ok

--- src/components/Profile/profile.jsx
+++ src/components/Profile/profile.jsx
@@ -11,15 +11,15 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
 
       <ul className="stats">
         <li>
-          <span className="label">followers</span>
+          <span className="label">Followers</span>
           <span className="quantity">{stats.followers}</span>
         </li>
         <li>
-          <span className="label">views</span>
+          <span className="label">Views</span>
           <span className="quantity">{stats.views}</span>
         </li>
         <li>
-          <span className="label">likes</span>
+          <span className="label">Likes</span>
           <span className="quantity">{stats.likes}</span>
         </li>
       </ul>
