d3ebe07891aee737916e3172f263f874ed4169e3
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-13 01:44:21 +0300

    2

--- src/components/Profile/profile.jsx
+++ src/components/Profile/profile.jsx
@@ -1,26 +1,26 @@
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   console.log(stats);
   return (
-    <div class="profile">
-      <div class="description">
-        <img src={avatar} alt="User avatar" class="avatar" />
-        <p class="name">{username}</p>
-        <p class="tag">@{tag}</p>
-        <p class="location">{location}</p>
+    <div className="profile">
+      <div className="description">
+        <img src={avatar} alt="User avatar" className="avatar" />
+        <p className="name">{username}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
       </div>
 
-      <ul class="stats">
+      <ul className="stats">
         <li>
-          <span class="label">followers</span>
-          <span class="quantity">1000</span>
+          <span className="label">followers</span>
+          <span className="quantity">{stats.followers}</span>
         </li>
         <li>
-          <span class="label">views</span>
-          <span class="quantity">2000</span>
+          <span className="label">views</span>
+          <span className="quantity">{stats.views}</span>
         </li>
         <li>
-          <span class="label">likes</span>
-          <span class="quantity">3000</span>
+          <span className="label">likes</span>
+          <span className="quantity">{stats.likes}</span>
         </li>
       </ul>
     </div>
