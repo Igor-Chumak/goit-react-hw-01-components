8becfc027d5054f673adabde903331026fdf817c
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-13 23:24:41 +0300

    4 begin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ import { Statistics } from './statistics/statistics';
 import data from '../data/data.json';
 import friends from '../data/friends.json';
 import { FriendList } from './friend_list/friendlist';
+import transactions from '../data/transactions.json';
+import { TransactionHistory } from './transaction_history/transaction_history';
 
 export const App = () => {
   return (
@@ -23,6 +25,7 @@ export const App = () => {
       <h2>3 - Friend list</h2>
       <FriendList friends={friends} />
       <h2>4 - Transaction history</h2>
+      <TransactionHistory items={transactions} />;
     </>
   );
 };
