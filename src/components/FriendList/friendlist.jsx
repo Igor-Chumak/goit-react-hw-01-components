ecb7d71b82230f2c43fe23b8997509eaa1bac05e
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-13 17:11:41 +0300

    3

--- src/components/FriendList/friendlist.jsx
+++ src/components/FriendList/friendlist.jsx
@@ -1,14 +1,23 @@
 import PropTypes from 'prop-types';
 
-export FriendList({friends, children}) => {
-    return (
-        <ul className="friend-list">
-            {children}
-        </ul>
-    )
-}
+export const FriendList = friends => {
+  return (
+    <ul className="friend-list">
+      {friends.map(friend => (
+        <li className="item">
+          <span className="status"></span>
+          <img className="avatar" src="" alt="User avatar" width="48" />
+          <p className="name"></p>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 FriendList.propTypes = {
-    friends: PropTypes.array,
-}
+  friends: PropTypes.array,
+};
 
+const FriendListItem = () => {
+  return;
+};
