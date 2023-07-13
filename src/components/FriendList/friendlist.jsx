86912f0be13c9d623eeae9fb64cade75af3272b7
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-13 22:53:37 +0300

    3 no

--- src/components/FriendList/friendlist.jsx
+++ src/components/FriendList/friendlist.jsx
@@ -1,43 +1,39 @@
-// import PropTypes from 'prop-types';
-
-export const FriendList = friends => {
-  return (
-    <ul className="friendlist">
-      {/* {friends.map(({ id, name }) => (
-        <li className="item" key={id}>
-          <span className="status"></span>
-          <img className="avatar" src="" alt="User avatar" width="48" />
-          <p className="name">{name}</p>
-        </li>
-      ))} */}
-    </ul>
-  );
-};
-
-// FriendList.propTypes = {
-//   friends: PropTypes.array,
-// };
-
-// const FriendListItem = () => {
-//   return;
-// };
+import PropTypes from 'prop-types';
 
 // export const FriendList = friends => {
-//   console.log(friends);
 //   return (
-//     <ul className="friend-list">
-//       {friends.map(friend => (
-//         <li className="item" key={friend.id}>
-//           <span className="status {friend.isOnline}"></span>
-//           <img
-//             className="avatar"
-//             src="{friend.avatar}"
-//             alt="User avatar"
-//             width="48"
-//           />
-//           <p className="name">{friend.name}</p>
+//     <ul className="friendlist">
+//       {friends.map(({ id, name }) => (
+//         <li className="item" key={id}>
+//           <span className="status"></span>
+//           <img className="avatar" src="" alt="User avatar" width="48" />
+//           <p className="name">{name}</p>
 //         </li>
 //       ))}
 //     </ul>
 //   );
 // };
+
+export const FriendList = friends => {
+  console.log(friends);
+  return (
+    <ul className="friend-list">
+      {friends.map(friend => (
+        <li className="item" key={friend.id}>
+          <span className="status {friend.isOnline}"></span>
+          <img
+            className="avatar"
+            src="{friend.avatar}"
+            alt="User avatar"
+            width="48"
+          />
+          <p className="name">{friend.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.array,
+};
