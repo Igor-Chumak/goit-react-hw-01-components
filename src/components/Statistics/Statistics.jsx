885bc8f82b658f1961fc7e9fd3863f32422cf06a
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-14 17:44:21 +0300

    2 css 1

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,7 +5,6 @@ export const Statistics = ({ title, stats }) => {
   return (
     <section className="statistics">
       {title && <h2 className="title">{title}</h2>}
-
       <ul className="statList">
         {stats.map(stat => (
           <li className="item" key={stat.id}>
