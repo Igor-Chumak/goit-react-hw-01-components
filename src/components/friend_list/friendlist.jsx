a27c7e01504213e22c6302097864626a9797942a
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-15 00:33:37 +0300

    4 1

--- src/components/friend_list/friendlist.jsx
+++ src/components/friend_list/friendlist.jsx
@@ -1,14 +1,37 @@
 import PropTypes from 'prop-types';
-// import style from './friendlist.module.css';
+import styles from './friendlist.module.css';
+// import styled from 'styled-components';
 
+// export const FriendList = ({ friends }) => {
+//   return (
+//     <ul className="friendList">
+//       {friends.map(({ id, avatar, name, isOnline }) => (
+//         <li className="item" key={id}>
+//           <span className={`status ${isOnline ? 'isOnline' : ''}`}></span>
+//           <img className="avatar" src={avatar} alt="User avatar" width="48" />
+//           <p className="name">{name}</p>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
+
+//     CSS module
 export const FriendList = ({ friends }) => {
   return (
-    <ul className="friendList">
+    <ul className={styles.friendList}>
       {friends.map(({ id, avatar, name, isOnline }) => (
-        <li className="item" key={id}>
-          <span className={`status ${isOnline ? 'isOnline' : ''}`}></span>
-          <img className="avatar" src={avatar} alt="User avatar" width="48" />
-          <p className="name">{name}</p>
+        <li className={styles.item} key={id}>
+          <span
+            className={`${styles.status} ${isOnline ? styles.isOnline : ''}`}
+          ></span>
+          <img
+            className={styles.avatar}
+            src={avatar}
+            alt="User avatar"
+            width="48"
+          />
+          <p className={styles.name}>{name}</p>
         </li>
       ))}
     </ul>
