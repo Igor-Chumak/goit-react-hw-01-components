819a2e14e916fada69339a26b031da77b9a68953
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-14 20:15:28 +0300

    css ren style

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,24 +1,24 @@
 import PropTypes from 'prop-types';
-import css from './statistics.module.css';
+import style from './statistics.module.css';
 import getRandomHexColor from '../js/random_hex_color.js';
 
 export const Statistics = ({ title, stats }) => {
   return (
-    <section className={css.statistics}>
+    <section className={style.statistics}>
       {title && (
-        <h2 className={css.title} style={{ color: getRandomHexColor() }}>
+        <h2 className={style.title} style={{ color: getRandomHexColor() }}>
           {title}
         </h2>
       )}
-      <ul className={css.statList}>
+      <ul className={style.statList}>
         {stats.map(stat => (
           <li
-            className={css.item}
+            className={style.item}
             style={{ backgroundColor: getRandomHexColor() }}
             key={stat.id}
           >
-            <span className={css.label}>{stat.label}</span>
-            <span className={css.percentage}>{stat.percentage}%</span>
+            <span className={style.label}>{stat.label}</span>
+            <span className={style.percentage}>{stat.percentage}%</span>
           </li>
         ))}
       </ul>
