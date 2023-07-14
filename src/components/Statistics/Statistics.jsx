1d6714008b403edbb10d8758bbaf88b908c35711
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-14 17:51:05 +0300

    css2 2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types';
 import css from './statistics.module.css';
+import getRandomHexColor from '../js/random_hex_color.js';
 
 export const Statistics = ({ title, stats }) => {
   return (
     <section className="statistics">
-      {title && <h2 className="title">{title}</h2>}
+      {title && (
+        <h2 className="title" style={{ color: getRandomHexColor() }}>
+          {title}
+        </h2>
+      )}
       <ul className="statList">
         {stats.map(stat => (
           <li className="item" key={stat.id}>
