49fb3fabe858fd7e8cfbcd32151218915e701821
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-17 12:01:21 +0300

    ppt stat

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -32,8 +32,8 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
 
 Profile.propTypes = {
   username: PropTypes.string.isRequired,
-  tag: PropTypes.string,
-  location: PropTypes.string,
-  avatar: PropTypes.string,
-  stats: PropTypes.object,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
 };
