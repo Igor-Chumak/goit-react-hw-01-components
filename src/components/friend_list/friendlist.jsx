eb531f4ff84d413f0785960b226c4b5b1db5f1e9
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-17 14:56:24 +0300

    3 emotion ok

--- src/components/friend_list/friendlist.jsx
+++ src/components/friend_list/friendlist.jsx
@@ -1,45 +1,53 @@
 import PropTypes from 'prop-types';
 
 //       Emotion
-// import styled from '@emotion/styled'
+
+import {
+  FriendsList,
+  FriendCard,
+  FriendAvatar,
+  FriendName,
+  FriendStatus,
+} from 'components/friend_list/friendlist.styled';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <FriendsList>
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <FriendCard key={id}>
+          <FriendStatus isOnline={isOnline}></FriendStatus>
+          <FriendAvatar src={avatar} alt="User avatar" width="48" />
+          <FriendName>{name}</FriendName>
+        </FriendCard>
+      ))}
+    </FriendsList>
+  );
+};
+
+//  CSS module
+
+// import css from './friendlist.module.css';
 
 // export const FriendList = ({ friends }) => {
 //   return (
-//     <ul className="friendList">
-//       {friends.map(({ id, avatar, name, isOnline }) => (
-//         <li className="item" key={id}>
-//           <span className={`status ${isOnline ? 'isOnline' : ''}`}></span>
-//           <img className="avatar" src={avatar} alt="User avatar" width="48" />
-//           <p className="name">{name}</p>
+//     <ul className={css.friendList}>
+//       {friends.map(({ id, avatar, name, isOnline = false }) => (
+//         <li className={css.item} key={id}>
+//           <span
+//             className={`${css.status} ${isOnline ? css.isOnline : ''}`}
+//           ></span>
+//           <img
+//             className={css.avatar}
+//             src={avatar}
+//             alt="User avatar"
+//             width="48"
+//           />
+//           <p className={css.name}>{name}</p>
 //         </li>
 //       ))}
 //     </ul>
 //   );
 // };
-
-//  CSS module
-import css from './friendlist.module.css';
-
-export const FriendList = ({ friends }) => {
-  return (
-    <ul className={css.friendList}>
-      {friends.map(({ id, avatar, name, isOnline = false }) => (
-        <li className={css.item} key={id}>
-          <span
-            className={`${css.status} ${isOnline ? css.isOnline : ''}`}
-          ></span>
-          <img
-            className={css.avatar}
-            src={avatar}
-            alt="User avatar"
-            width="48"
-          />
-          <p className={css.name}>{name}</p>
-        </li>
-      ))}
-    </ul>
-  );
-};
 
 FriendList.propTypes = {
   friends: PropTypes.arrayOf(
